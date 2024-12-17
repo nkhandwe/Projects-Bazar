f77f408a19c3cc9216cb6316c5eea460b49203ed
Narendra Khandwe <nkhandwe@gmail.com>
2024-12-17 13:26:26 +0530

    Update

--- resources/themes/default/web-views/partials/_category-section-home.blade.php
+++ resources/themes/default/web-views/partials/_category-section-home.blade.php
@@ -1,4 +1,4 @@
-{{-- @if ($categories->count() > 0 )
+@if ($categories->count() > 0)
     <section class="pb-4 rtl">
         <div class="container">
             <div>
@@ -6,26 +6,29 @@
                     <div class="card-body">
                         <div class="d-flex justify-content-between">
                             <div class="categories-title m-0">
-                                <span class="font-semibold">{{ translate('categories')}}</span>
+                                <span class="font-semibold">{{ translate('categories') }}</span>
                             </div>
                             <div>
                                 <a class="text-capitalize view-all-text web-text-primary"
-                                   href="{{route('categories')}}">{{ translate('view_all')}}
-                                    <i class="czi-arrow-{{Session::get('direction') === "rtl" ? 'left mr-1 ml-n1 mt-1 float-left' : 'right ml-1 mr-n1'}}"></i>
+                                    href="{{ route('categories') }}">{{ translate('view_all') }}
+                                    <i
+                                        class="czi-arrow-{{ Session::get('direction') === 'rtl' ? 'left mr-1 ml-n1 mt-1 float-left' : 'right ml-1 mr-n1' }}"></i>
                                 </a>
                             </div>
                         </div>
                         <div class="d-none d-md-block">
                             <div class="row mt-3">
-                                @foreach($categories as $key => $category)
-                                    @if ($key<10)
+                                @foreach ($categories as $key => $category)
+                                    @if ($key < 10)
                                         <div class="text-center __m-5px __cate-item">
-                                            <a href="{{route('products',['category_id'=> $category['id'],'data_from'=>'category','page'=>1])}}">
+                                            <a
+                                                href="{{ route('products', ['category_id' => $category['id'], 'data_from' => 'category', 'page' => 1]) }}">
                                                 <div class="__img">
                                                     <img alt="{{ $category->name }}"
-                                                         src="{{ getStorageImages(path:$category->icon_full_url, type: 'category') }}">
+                                                        src="{{ getStorageImages(path: $category->icon_full_url, type: 'category') }}">
                                                 </div>
-                                                <p class="text-center fs-13 font-semibold mt-2">{{Str::limit($category->name, 12)}}</p>
+                                                <p class="text-center fs-13 font-semibold mt-2">
+                                                    {{ Str::limit($category->name, 12) }}</p>
                                             </a>
                                         </div>
                                     @endif
@@ -34,15 +37,17 @@
                         </div>
                         <div class="d-md-none">
                             <div class="owl-theme owl-carousel categories--slider mt-3">
-                                @foreach($categories as $key => $category)
-                                    @if ($key<10)
+                                @foreach ($categories as $key => $category)
+                                    @if ($key < 10)
                                         <div class="text-center m-0 __cate-item w-100">
-                                            <a href="{{route('products',['category_id'=> $category['id'],'data_from'=>'category','page'=>1])}}">
+                                            <a
+                                                href="{{ route('products', ['category_id' => $category['id'], 'data_from' => 'category', 'page' => 1]) }}">
                                                 <div class="__img mw-100 h-auto">
                                                     <img alt="{{ $category->name }}"
-                                                         src="{{ getStorageImages(path: $category->icon_full_url, type: 'category') }}">
+                                                        src="{{ getStorageImages(path: $category->icon_full_url, type: 'category') }}">
                                                 </div>
-                                                <p class="text-center small mt-2">{{Str::limit($category->name, 12)}}</p>
+                                                <p class="text-center small mt-2">{{ Str::limit($category->name, 12) }}
+                                                </p>
                                             </a>
                                         </div>
                                     @endif
@@ -54,4 +59,4 @@
             </div>
         </div>
     </section>
-@endif --}}
+@endif
