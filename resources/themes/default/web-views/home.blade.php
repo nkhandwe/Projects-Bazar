ea54cf127d4705a4988e14d75e794f332d984428
Narendra Khandwe <nkhandwe@gmail.com>
2024-12-18 14:32:46 +0530

    Updated Code for product card design

--- resources/themes/default/web-views/home.blade.php
+++ resources/themes/default/web-views/home.blade.php
@@ -1,59 +1,69 @@
 @extends('layouts.front-end.app')
 
-@section('title', $web_config['name']->value.' '.translate('online_Shopping').' | '.$web_config['name']->value.' '.translate('ecommerce'))
+@section('title', $web_config['name']->value . ' ' . translate('online_Shopping') . ' | ' . $web_config['name']->value .
+    ' ' . translate('ecommerce'))
 
-@push('css_or_js')
-    <meta property="og:image" content="{{$web_config['web_logo']['path']}}"/>
-    <meta property="og:title" content="Welcome To {{$web_config['name']->value}} Home"/>
-    <meta property="og:url" content="{{env('APP_URL')}}">
-    <meta property="og:description" content="{{ substr(strip_tags(str_replace('&nbsp;', ' ', $web_config['about']->value)),0,160) }}">
+    @push('css_or_js')
+        <meta property="og:image" content="{{ $web_config['web_logo']['path'] }}" />
+        <meta property="og:title" content="Welcome To {{ $web_config['name']->value }} Home" />
+        <meta property="og:url" content="{{ env('APP_URL') }}">
+        <meta property="og:description"
+            content="{{ substr(strip_tags(str_replace('&nbsp;', ' ', $web_config['about']->value)), 0, 160) }}">
 
-    <meta property="twitter:card" content="{{$web_config['web_logo']['path']}}"/>
-    <meta property="twitter:title" content="Welcome To {{$web_config['name']->value}} Home"/>
-    <meta property="twitter:url" content="{{env('APP_URL')}}">
-    <meta property="twitter:description" content="{{ substr(strip_tags(str_replace('&nbsp;', ' ', $web_config['about']->value)),0,160) }}">
+        <meta property="twitter:card" content="{{ $web_config['web_logo']['path'] }}" />
+        <meta property="twitter:title" content="Welcome To {{ $web_config['name']->value }} Home" />
+        <meta property="twitter:url" content="{{ env('APP_URL') }}">
+        <meta property="twitter:description"
+            content="{{ substr(strip_tags(str_replace('&nbsp;', ' ', $web_config['about']->value)), 0, 160) }}">
 
-    <link rel="stylesheet" href="{{theme_asset(path: 'public/assets/front-end/css/home.css')}}"/>
-    <link rel="stylesheet" href="{{ theme_asset(path: 'public/assets/front-end/css/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ theme_asset(path: 'public/assets/front-end/css/owl.theme.default.min.css') }}">
-@endpush
+        <link rel="stylesheet" href="{{ theme_asset(path: 'public/assets/front-end/css/home.css') }}" />
+        <link rel="stylesheet" href="{{ theme_asset(path: 'public/assets/front-end/css/owl.carousel.min.css') }}">
+        <link rel="stylesheet" href="{{ theme_asset(path: 'public/assets/front-end/css/owl.theme.default.min.css') }}">
+    @endpush
 
 @section('content')
     <div class="__inline-61">
         @php($decimalPointSettings = !empty(getWebConfig(name: 'decimal_point_settings')) ? getWebConfig(name: 'decimal_point_settings') : 0)
 
-        @include('web-views.partials._home-top-slider',['main_banner'=>$main_banner])
+        @include('web-views.partials._home-top-slider', ['main_banner' => $main_banner])
 
         @if ($flashDeal['flashDeal'] && $flashDeal['flashDealProducts'] && count($flashDeal['flashDealProducts']) > 0)
-            @include('web-views.partials._flash-deal', ['decimal_point_settings'=>$decimalPointSettings])
+            @include('web-views.partials._flash-deal', ['decimal_point_settings' => $decimalPointSettings])
         @endif
 
-        @if ($featuredProductsList->count() > 0 )
+        @if ($featuredProductsList->count() > 0)
             <div class="container py-4 rtl px-0 px-md-3">
                 <div class="__inline-62 pt-3">
                     <div class="feature-product-title mt-0 web-text-primary">
                         {{ translate('featured_products') }}
                     </div>
                     <div class="text-end px-3 d-none d-md-block">
-                        <a class="text-capitalize view-all-text web-text-primary" href="{{route('products',['data_from'=>'featured','page'=>1])}}">
-                            {{ translate('view_all')}}
-                            <i class="czi-arrow-{{Session::get('direction') === 'rtl' ? 'left mr-1 ml-n1 mt-1' : 'right ml-1'}}"></i>
+                        <a class="text-capitalize view-all-text web-text-primary"
+                            href="{{ route('products', ['data_from' => 'featured', 'page' => 1]) }}">
+                            {{ translate('view_all') }}
+                            <i
+                                class="czi-arrow-{{ Session::get('direction') === 'rtl' ? 'left mr-1 ml-n1 mt-1' : 'right ml-1' }}"></i>
                         </a>
                     </div>
                     <div class="feature-product">
                         <div class="carousel-wrap p-1">
                             <div class="owl-carousel owl-theme" id="featured_products_list">
-                                @foreach($featuredProductsList as $product)
+                                @foreach ($featuredProductsList as $product)
                                     <div>
-                                        @include('web-views.partials._feature-product',['product'=>$product, 'decimal_point_settings'=>$decimalPointSettings])
+                                        @include('web-views.partials._feature-product', [
+                                            'product' => $product,
+                                            'decimal_point_settings' => $decimalPointSettings,
+                                        ])
                                     </div>
                                 @endforeach
                             </div>
                         </div>
                         <div class="text-center pt-2 d-md-none">
-                            <a class="text-capitalize view-all-text web-text-primary" href="{{route('products',['data_from'=>'featured','page'=>1])}}">
-                                {{ translate('view_all')}}
-                                <i class="czi-arrow-{{Session::get('direction') === "rtl" ? 'left mr-1 ml-n1 mt-1' : 'right ml-1'}}"></i>
+                            <a class="text-capitalize view-all-text web-text-primary"
+                                href="{{ route('products', ['data_from' => 'featured', 'page' => 1]) }}">
+                                {{ translate('view_all') }}
+                                <i
+                                    class="czi-arrow-{{ Session::get('direction') === 'rtl' ? 'left mr-1 ml-n1 mt-1' : 'right ml-1' }}"></i>
                             </a>
                         </div>
                     </div>
@@ -63,7 +73,7 @@
 
         @include('web-views.partials._category-section-home')
 
-        @if($web_config['featured_deals'] && (count($web_config['featured_deals'])>0))
+        {{-- @if ($web_config['featured_deals'] && count($web_config['featured_deals']) > 0)
             <section class="featured_deal">
                 <div class="container">
                     <div class="__featured-deal-wrap bg--light">
@@ -81,42 +91,38 @@
                             </div>
                         </div>
                         <div class="owl-carousel owl-theme new-arrivals-product">
-                            @foreach($web_config['featured_deals'] as $key=>$product)
+                            @foreach ($web_config['featured_deals'] as $key => $product)
                                 @include('web-views.partials._product-card-1',['product'=>$product, 'decimal_point_settings'=>$decimalPointSettings])
                             @endforeach
                         </div>
                     </div>
                 </div>
             </section>
-        @endif
+        @endif --}}
 
         @if (isset($main_section_banner))
             <div class="container rtl pt-4 px-0 px-md-3">
-                <a href="{{$main_section_banner->url}}" target="_blank"
-                    class="cursor-pointer d-block">
+                <a href="{{ $main_section_banner->url }}" target="_blank" class="cursor-pointer d-block">
                     <img class="d-block footer_banner_img __inline-63" alt=""
-                         src="{{ getStorageImages(path:$main_section_banner->photo_full_url, type: 'wide-banner') }}">
+                        src="{{ getStorageImages(path: $main_section_banner->photo_full_url, type: 'wide-banner') }}">
                 </a>
             </div>
         @endif
 
-        @php($businessMode = getWebConfig(name: 'business_mode'))
+        {{-- @php($businessMode = getWebConfig(name: 'business_mode'))
         @if ($businessMode == 'multi' && count($topVendorsList) > 0)
             @include('web-views.partials._top-sellers')
-        @endif
+        @endif --}}
 
-        @include('web-views.partials._deal-of-the-day', ['decimal_point_settings'=>$decimalPointSettings])
+        {{-- @include('web-views.partials._deal-of-the-day', ['decimal_point_settings'=>$decimalPointSettings]) --}}
 
         <section class="new-arrival-section">
 
-            @if ($newArrivalProducts->count() >0 )
+            @if ($newArrivalProducts->count() > 0)
                 <div class="container rtl mt-4">
-                    <div class="section-header">
-                        <div class="arrival-title d-block">
-                            <div class="text-capitalize">
-                                {{ translate('new_arrivals')}}
-                            </div>
-                        </div>
+                    <div class="text-center mt-5 mb-5" style="margin-bottom: 100px!important;">
+                        <h1><strong>New <span style="color: #5f3dc4;">Arrivals</span></strong></h1>
+                        <span class="mb-5">Our latest offerings</span>
                     </div>
                 </div>
                 <div class="container rtl mb-3 overflow-hidden">
@@ -124,8 +130,11 @@
                         <div class="new_arrival_product">
                             <div class="carousel-wrap">
                                 <div class="owl-carousel owl-theme new-arrivals-product">
-                                    @foreach($newArrivalProducts as $key=> $product)
-                                        @include('web-views.partials._product-card-2',['product'=>$product,'decimal_point_settings'=>$decimalPointSettings])
+                                    @foreach ($newArrivalProducts as $key => $product)
+                                        @include('web-views.partials._product-card-2', [
+                                            'product' => $product,
+                                            'decimal_point_settings' => $decimalPointSettings,
+                                        ])
                                     @endforeach
                                 </div>
                             </div>
@@ -134,17 +143,13 @@
                 </div>
             @endif
 
-            <div class="container rtl px-0 px-md-3">
-                <div class="row g-3 mx-max-md-0">
 
-                    @if ($bestSellProduct->count() >0)
-                        @include('web-views.partials._best-selling')
-                    @endif
+        </section>
 
-                    @if ($topRated->count() >0)
-                        @include('web-views.partials._top-rated')
-                    @endif
-                </div>
+        <section class="interest-section">
+            <div class="text-center mt-5 mb-5" style="margin-bottom: 100px!important;">
+                <h1><strong>SHOP BY <span style="color: #5f3dc4;">INTEREST</span></strong></h1>
+                <span class="mb-5">A whole lotta fun & learning</span>
             </div>
         </section>
 
@@ -152,9 +157,10 @@
         @if (count($footer_banner) > 1)
             <div class="container rtl pt-4">
                 <div class="promotional-banner-slider owl-carousel owl-theme">
-                    @foreach($footer_banner as $banner)
+                    @foreach ($footer_banner as $banner)
                         <a href="{{ $banner['url'] }}" class="d-block" target="_blank">
-                            <img class="footer_banner_img __inline-63"  alt="" src="{{ getStorageImages(path:$banner->photo_full_url, type: 'banner') }}">
+                            <img class="footer_banner_img __inline-63" alt=""
+                                src="{{ getStorageImages(path: $banner->photo_full_url, type: 'banner') }}">
                         </a>
                     @endforeach
                 </div>
@@ -162,10 +168,11 @@
         @else
             <div class="container rtl pt-4">
                 <div class="row">
-                    @foreach($footer_banner as $banner)
+                    @foreach ($footer_banner as $banner)
                         <div class="col-md-6">
                             <a href="{{ $banner['url'] }}" class="d-block" target="_blank">
-                                <img class="footer_banner_img __inline-63"  alt="" src="{{ getStorageImages(path:$banner->photo_full_url, type: 'banner') }}">
+                                <img class="footer_banner_img __inline-63" alt=""
+                                    src="{{ getStorageImages(path: $banner->photo_full_url, type: 'banner') }}">
                             </a>
                         </div>
                     @endforeach
@@ -173,7 +180,7 @@
             </div>
         @endif
 
-        @if($web_config['brand_setting'] && $brands->count() > 0)
+        {{-- @if ($web_config['brand_setting'] && $brands->count() > 0)
             <section class="container rtl pt-4">
 
                 <div class="section-header">
@@ -190,7 +197,7 @@
 
                 <div class="mt-sm-3 mb-3 brand-slider">
                     <div class="owl-carousel owl-theme p-2 brands-slider">
-                        @foreach($brands as $brand)
+                        @foreach ($brands as $brand)
                             <div class="text-center">
                                 <a href="{{route('products',['brand_id'=> $brand['id'],'data_from'=>'brand','page'=>1])}}"
                                    class="__brand-item">
@@ -202,25 +209,24 @@
                     </div>
                 </div>
             </section>
-        @endif
+        @endif --}}
 
-        @if ($homeCategories->count() > 0)
-            @foreach($homeCategories as $category)
+        {{-- @if ($homeCategories->count() > 0)
+            @foreach ($homeCategories as $category)
                 @include('web-views.partials._category-wise-product', ['decimal_point_settings'=>$decimalPointSettings])
             @endforeach
-        @endif
+        @endif --}}
 
-        @php($companyReliability = getWebConfig(name: 'company_reliability'))
-        @if($companyReliability != null)
+        {{-- @php($companyReliability = getWebConfig(name: 'company_reliability'))
+        @if ($companyReliability != null)
             @include('web-views.partials._company-reliability')
-        @endif
+        @endif --}}
     </div>
 
     <span id="direction-from-session" data-value="{{ session()->get('direction') }}"></span>
 @endsection
 
 @push('script')
-    <script src="{{theme_asset(path: 'public/assets/front-end/js/owl.carousel.min.js')}}"></script>
+    <script src="{{ theme_asset(path: 'public/assets/front-end/js/owl.carousel.min.js') }}"></script>
     <script src="{{ theme_asset(path: 'public/assets/front-end/js/home.js') }}"></script>
 @endpush
-
