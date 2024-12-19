a6d76c8cb5040f0a8f24ec7be5eedcdffc963d81
Narendra Khandwe <nkhandwe@gmail.com>
2024-12-19 10:47:08 +0530

    Updated code for pills active and all

--- resources/themes/default/web-views/partials/_product-card-2.blade.php
+++ resources/themes/default/web-views/partials/_product-card-2.blade.php
@@ -1,6 +1,6 @@
 @if(isset($product))
     @php($overallRating = getOverallRating($product->reviews))
-    <div class="flash_deal_product get-view-by-onclick card p-1 mb-3" style="box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); border-radius: 8px;" data-link="{{ route('product',$product->slug) }}">
+    <div class="flash_deal_product get-view-by-onclick card p-1 mb-3 me-2" style="box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); border-radius: 8px;" data-link="{{ route('product',$product->slug) }}">
         <div>
             <div class=" mb-3 position-relative">
                 <img class="img-fluid w-100" style="max-height: 300px; object-fit: cover; border-radius: 8px;" alt="" src="{{ getStorageImages(path: $product->thumbnail_full_url, type: 'product') }}">
@@ -29,7 +29,7 @@
                 </div>
             @endif
 
-            <div class="mb-3 p-3">
+            <div class="mb-3 p-1">
                   @if($product->discount > 0)
                     <span class="badge badge-danger shadow" style="font-size: 14px; border-radius:50px;">
                         @if ($product->discount_type == 'percent')
