712b24c9390f1e679cabd8591342aa3af57ac673
Narendra Khandwe <nkhandwe@gmail.com>
2024-12-20 14:01:20 +0530

    updated page

--- resources/themes/default/web-views/partials/_new-arrival.blade.php
+++ resources/themes/default/web-views/partials/_new-arrival.blade.php
@@ -1,5 +1,4 @@
        <!-- Include Bootstrap and Owl Carousel CSS/JS -->
-       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
        <link rel="stylesheet"
            href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
