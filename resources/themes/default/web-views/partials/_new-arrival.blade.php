bd824709c41ec601b45e868f9553ef9f7fd35b0e
Narendra Khandwe <nkhandwe@gmail.com>
2024-12-30 09:15:57 +0530

    Update

--- resources/themes/default/web-views/partials/_new-arrival.blade.php
+++ resources/themes/default/web-views/partials/_new-arrival.blade.php
@@ -39,35 +39,35 @@
 
        <section class="interest-section">
            <div class="text-center mt-5 mb-5" style="margin-bottom: 100px!important;">
-               <h1><strong>SHOP BY <span class="text-primary">INTEREST</span></strong></h1>
+               <h1><strong>SHOP BY <span class="text-primary">SUBCATEGORY</span></strong></h1>
                <span class="mb-5">A whole lotta fun & learning</span>
            </div>
+           @php($subcategories = \App\Models\Category::where('parent_id', '!=', 0)->get())
 
            <div class="container rtl pb-4 px-max-sm-0">
-               <ul class="nav nav-pills mb-3" id="categoryTabs" role="tablist">
-                   @foreach ($categories as $key => $category)
+               <ul class="nav nav-pills mb-3" id="subcategoryTabs" role="tablist">
+                   @foreach ($subcategories as $key => $subcategory)
                        <li class="nav-item" role="presentation">
                            <button class="nav-link {{ $key === 0 ? 'active' : '' }}"
-                               id="category-tab-{{ $category->id }}" data-bs-toggle="tab"
-                               data-bs-target="#category-{{ $category->id }}" type="button" role="tab"
-                               aria-controls="category-{{ $category->id }}"
+                               id="subcategory-tab-{{ $subcategory->id }}" data-bs-toggle="tab"
+                               data-bs-target="#subcategory-{{ $subcategory->id }}" type="button" role="tab"
+                               aria-controls="subcategory-{{ $subcategory->id }}"
                                aria-selected="{{ $key === 0 ? 'true' : 'false' }}">
-                               {{ $category->name }}
+                               {{ $subcategory->name }}
                            </button>
                        </li>
                    @endforeach
                </ul>
-
-               <div class="tab-content" id="categoryTabsContent">
-                   @foreach ($categories as $key => $category)
+               <div class="tab-content" id="subcategoryTabsContent">
+                   @foreach ($subcategories as $key => $subcategory)
                        <div class="tab-pane fade {{ $key === 0 ? 'show active' : '' }}"
-                           id="category-{{ $category->id }}" role="tabpanel"
-                           aria-labelledby="category-tab-{{ $category->id }}">
-                           @if (count($category->product) > 0)
+                           id="subcategory-{{ $subcategory->id }}" role="tabpanel"
+                           aria-labelledby="subcategory-tab-{{ $subcategory->id }}">
+                           @if ($subcategory->subCategoryProduct->count() > 0)
                                <div class="__p-20px rounded overflow-hidden">
                                    <div class="mt-2">
                                        <div class="owl-carousel owl-theme new-arrivals-product">
-                                           @foreach ($newArrivalProducts as $key => $product)
+                                           @foreach ($subcategory->subCategoryProduct as $key => $product)
                                                @include('web-views.partials._product-card-2', [
                                                    'product' => $product,
                                                    'decimal_point_settings' => $decimalPointSettings,
@@ -77,7 +77,7 @@
 
                                        <div class="d-sm-none">
                                            <div class="row g-2">
-                                               @foreach ($category->product as $key => $product)
+                                               @foreach ($subcategory->subCategoryProduct as $key => $product)
                                                    @if ($key < 4)
                                                        <div class="col-6">
                                                            @include('web-views.partials._product-card-2', [
@@ -94,8 +94,8 @@
                            @else
                                <div class="text-center mt-5 mb-5" style="margin-bottom: 100px!important;">
                                    <h1><strong>No products found <span class="text-primary">in this
-                                               category.</span></strong></h1>
-                                   <span class="mb-5">Please Check with another category!</span>
+                                               subcategory.</span></strong></h1>
+                                   <span class="mb-5">Please Check with another Category!</span>
                                </div>
                            @endif
                        </div>
